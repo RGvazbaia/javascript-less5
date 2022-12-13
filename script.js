df4219d5325edd1796c264a4e727c33e61e84180
Rati Gvazbaia <rgvazbaia@gmail.com>
2022-12-13 17:25:42 +0400

    add script

---
+++ script.js
@@ -0,0 +1,37 @@
+// davaleba 1
+
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+// let arr4 = arr1.concat(arr2,arr3);
+// console.log(arr4);
+
+
+
+// davaleba2
+
+// let arr = [-1, -2, -3, 4];
+
+// arr.some(positive);
+// function positive(numb) {
+//   console.log(numb > 0);
+// }
+
+
+// davaleba 3                                 ???
+// let array = [23,45,32,5,87,7,3,98];
+// array.sort();
+// console.log(array);
+
+
+// davaleba 4
+
+let img=document.createElement('img');
+img.src = "ferrari.jpg";
+document.querySelector("#wraper").appendChild(img);
+
+let h2=document.createElement('h2');
+h2.innerText = "Ferrari";
+document.querySelector("#wraper"). appendChild(h2);
+h2.style.color = "red";
+h2.style.fontSize = "30px";
